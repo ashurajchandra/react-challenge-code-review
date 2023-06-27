79567ad0df6c0667002fa2da286dddf8853163a3
ashurajchandra <mukutrai12@gmail.com>
2023-06-27 10:22:30 +0530

    added redo and undo logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
 function App() {
+  const [mouseCordinates, setMouseCordinates] = useState([])
+
+  const handleMouseClick = (e)=>{
+    //x & y co-ordinate
+    console.log("e",e)
+    const {clientX, clientY} = e
+    //prevState
+    //maintain the preveious clicked co-ordinates as well as new co-ordiantes
+    // setMouseCordinates([...mouseCordinates,{x:clientX, y:clientY}])
+
+    setMouseCordinates((prevState)=>{
+      const newCordinates = [...prevState]
+      newCordinates.push({x:clientX, y:clientY})
+      return newCordinates
+    })
+
+
+  }
+
+  const handleUndo = ()=>{
+    //remove last element from array
+    const localCordinates = [...mouseCordinates]
+    localCordinates.pop()
+    console.log("mouseCordinates",mouseCordinates)
+    setMouseCordinates(localCordinates)
+  }
+
+  console.log("mouseCordinates",mouseCordinates)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <React.Fragment>
+         <button onClick={handleUndo}>
+          Undo
+        </button>
+        <button>
+          Redo
+        </button>
+   
+    <div className="App" onClick={handleMouseClick}>
+      <div className="action-buttons">
+        {/* <button>
+          Undo
+        </button>
+        <button>
+          Redo
+        </button> */}
+      </div>
+   {/* <h1>Hello react</h1> */}
+  {
+    mouseCordinates.length>0  && mouseCordinates.map((item,index)=><div style={{left:item.x-10 , top: item.y-10}} key={index} className="cordinate">{index + 1}</div>)
+  }
+  
     </div>
+    </React.Fragment>
   );
 }
 
